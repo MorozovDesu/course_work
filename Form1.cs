970f30b7d845df6968389df2ca0731746eff88fb
ChoppaDMI <54062157+ChoppaDMI@users.noreply.github.com>
2022-11-22 19:40:40 +0800

    работает

--- Form1.cs
+++ Form1.cs
@@ -32,5 +32,26 @@ namespace course_work
 
 
         }
+        int counter = 0; // добавлю счетчик чтобы считать вызовы функции
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            counter++; // увеличиваю значение счетчика каждый вызов
+            using (var g = Graphics.FromImage(picDisplay.Image))
+            {
+                g.Clear(Color.White); // добавил очистку
+                // рисую на изображении сколько насчитал
+                g.DrawString(
+                    counter.ToString(), // значения счетчика в виде строки
+                    new Font("Arial", 12), // шрифт
+                    new SolidBrush(Color.Black), // цвет
+                    new PointF
+                    { // по центру экрана
+                        X = picDisplay.Image.Width / 2,
+                        Y = picDisplay.Image.Height / 2
+                    }
+                );
+            }
+            picDisplay.Invalidate();
+        }
     }
 }
