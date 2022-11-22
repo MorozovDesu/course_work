4001f26e2676f162ba29cbcd801f8cc3b1a007d4
ChoppaDMI <54062157+ChoppaDMI@users.noreply.github.com>
2022-11-22 19:24:47 +0800

    работает

--- Form1.cs
+++ Form1.cs
@@ -12,10 +12,25 @@ namespace course_work
 {
     public partial class Form1 : Form
     {
+        // собственно список, пока пустой
+        List<Particle> particles = new List<Particle>();
         public Form1()
         {
             InitializeComponent();
             picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
+
+            // генерирую 500 частиц
+            for (var i = 0; i < 500; ++i)
+            {
+                var particle = new Particle();
+                // переношу частицы в центр изображения
+                particle.X = picDisplay.Image.Width / 2;
+                particle.Y = picDisplay.Image.Height / 2;
+                // добавляю список
+                particles.Add(particle);
+            }
+
+
         }
     }
 }
